2a9025b50eb5735e0fc0ce4a8e59cc22d58be4e4
anthonyriesen <anthonyriesen@192.168.1.4>
2017-12-17 15:39:53 -0700

    Some parameters added to the ToDoTask object.
    Corresponding tests have not yet been added.

--- src/main/java/model/ToDoTask.java
+++ src/main/java/model/ToDoTask.java
@@ -2,13 +2,26 @@ package model;
 
 public class ToDoTask {
 	private String description;
+	// private datetime dueDate;
+	private Boolean noDate;
+	// private enum catagory;
+	// private enum priority;
+	private String details;
+
 
 	/**
 	 * Basic Task Class Object for data organization
-	 * @param description
+	 * @param description A string simply describing the task
+	 * @param dueDate A datetime object that describes when a task is due
+	 * @param noDate A boolean determining whether a due date is set
+	 * @param catagory An enumerate option that fits the task to a category
+	 * @param priority An integer that allows
+	 * @param detials
 	 */
-	public ToDoTask(String description) {
+	public ToDoTask(String description, boolean noDate, String details) {
 		this.setDescription(description);
+		this.setNoDate(noDate);
+		this.setDetails(details);
 	}
 
 	public String getDescription() {
@@ -17,5 +30,21 @@ public class ToDoTask {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	
+	public boolean getNoDate() {
+		return noDate;
+	}
+	
+	public void setNoDate(Boolean noDate) {
+		this.noDate = noDate;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
 	}
 }
